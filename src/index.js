3b882f3987c04610c0f4c99c7d82d40517932256
seungju <kingsky32@gmail.com>
2020-08-02 23:55:46 +0900

    Actions

--- src/index.js
+++ src/index.js
@@ -4,7 +4,12 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-const countModifier = (count = 0) => {
+const countModifier = (count = 0, action) => {
+  if (action.type === "Add") {
+    count++;
+  } else if (action.type === "Minus") {
+    count--;
+  }
   return count;
 };
 
