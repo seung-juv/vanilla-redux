2381c7b7dd8bc0d421c9d721de576eadb60a7f89
seungju <kingsky32@gmail.com>
2020-08-03 00:01:23 +0900

    Subscriptions

--- src/index.js
+++ src/index.js
@@ -14,3 +14,20 @@ const countModifier = (count = 0, action) => {
 };
 
 const countStore = createStore(countModifier);
+
+const onChange = () => {
+  number.innerHTML = countStore.getState();
+};
+
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+  countStore.dispatch({ type: "Add" });
+};
+
+const handleMinus = () => {
+  countStore.dispatch({ type: "Minus" });
+};
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
